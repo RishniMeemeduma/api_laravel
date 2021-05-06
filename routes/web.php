476710626a907d71422c9c806df91af6313598f1
Rishni <ashameemeduma@gmail.com>
2021-05-06 20:42:27 +0530

    add

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::post('/api/v1/bet','App\Http\Controllers\api\v1\betController@show')->name('show');
-//Route::resource('betController','App\Http\Controllers\api\v1\betController');
-//Route::resource('bet','App\Http\Controllers\api\v1\betController');
-//Route::resource('balancetransactionController','App\Http\Controllers\api\v1\balancetransactionController');
-//Route::resource('playerController','App\Http\Controllers\api\v1\playerController');
+Route::resource('betController','App\Http\Controllers\api\v1\betController');
+Route::resource('bet','App\Http\Controllers\api\v1\betController');
+Route::resource('balancetransactionController','App\Http\Controllers\api\v1\balancetransactionController');
+Route::resource('playerController','App\Http\Controllers\api\v1\playerController');
 //Route::resource('betController/store','betController@store_bet')->name('betController.store_bet');
